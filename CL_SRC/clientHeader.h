4f6807f5de91846f033f0a5ff8f20f0091bb114f
qu3b411 <jmohrbutter@gmail.com>
2021-01-09 06:40:13 -0600

    bug squashing

--- CL_SRC/clientHeader.h
+++ CL_SRC/clientHeader.h
@@ -95,8 +95,11 @@ __attribute__((section(".payload"))) BYTE* recv_secure();
  * without seeing all of the behind the sceans logic, this will be my next step after getting the encrypted
  * comms to a working state.
  */
+#ifndef SESSIONREQ
+#define SESSIONREQ
 BYTE* SessionIV;
 BYTE* SessionKEY;
+#endif 
 #ifdef WIN32
 
 	BCRYPT_KEY_HANDLE SessionKeyHandle;
