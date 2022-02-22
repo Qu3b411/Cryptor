068e79d61171639bf913f32df63779a0b9a309d5
Qu3b411 <19296119+Qu3b411@users.noreply.github.com>
2022-02-21 21:15:20 -0600

    Delete CommandAndControl.py

--- tmp/server/CommandAndControl.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import server as server; 
-class victim_payload(server.victim):
-    def __init__(self):
-        super().__init__()
-    def payload(self):
-        #Your payload goes here
-v = victim_payload();
-v.payload()
